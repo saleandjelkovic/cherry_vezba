41e736f53d2d62d50fab6dc8cf5b0ca1017de9ac
Sasa993 <jovicic.sasa@hotmail.com>
2019-05-02 22:03:41 +0200

    changed navbar login/logout

--- books/models.py
+++ books/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.forms import ModelForm
 
+
 class Author(models.Model):
 	name = models.CharField(max_length=100)
 	last_name = models.CharField(max_length=100)
